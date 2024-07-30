c2d33b8abb0cabe3fa916e59c3c7f65abd37aeec
diang <md.diang11@gmail.com>
2024-07-30 14:51:18 +0200

    mis a jour de la branche authenticate

--- Controllers/authGuard.js
+++ Controllers/authGuard.js
@@ -5,12 +5,10 @@ const authGuard = (req, res, next) => {
     const authHeader = req.headers['authorization'];
 
     if (!authHeader) {
-        console.log("No token provided");
         return res.status(403).json({ error: 'No token provided' });
     }
 
     const token = authHeader.split(' ')[1];
-    console.log("Token:", token);
 
     if (!token) {
         return res.status(403).json({ error: 'Invalid token format' });
