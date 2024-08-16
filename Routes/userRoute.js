6776811f492a3ba47125d5ec0a3d46cbab430fb0
diang <md.diang11@gmail.com>
2024-08-16 07:49:53 +0200

    rectification de recuperation des roles

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', (req, res) => userController.getAllUser(req,res));
 router.get('/:id', (req, res) => userController.getUserById(req,res));
 router.post('/', (req, res) => userController.addUser(req,res));
-router.get('/role/roleName', (res, req) => userController.getUserByRole(req, res));
+router.get('/role/:roleName', (req, res) => userController.getUserByRole(req, res));
 
 
 module.exports = router;
