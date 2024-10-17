2af1771db20ac84cb53b374e8e4e10f3d188dcbc
Mariepo <marie.potte.62@gmail.com>
2024-10-17 15:46:31 +0200

    ajout de l'overview de l'évaluation

--- Services/moduleService.js
+++ Services/moduleService.js
@@ -1,8 +1,14 @@
 const Module = require("../Models/module");
+const User = require("../Models/user");
 
 class ModuleService {
     async getModuleById (moduleId) {
-        return await Module.findByPk(moduleId)
+        return await Module.findByPk(moduleId, {
+            include:[{
+                model: User, 
+                as: 'formateur'
+            }] 
+        })
     }
 }
 
